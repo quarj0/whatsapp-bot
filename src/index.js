043ff49c8c8c9ae8ac6ce6ad58c633579bba0e2e
quarj0 <owusuansahkwadwo24@gmail.com>
2025-05-17 22:49:02 +0000

    Add HTTP server for Render

--- src/index.js
+++ src/index.js
@@ -20,10 +20,10 @@ const client = new Client({
 let botJid = null;
 let adminJid = null;
 
-client.on('qr', qr => {
-  console.log('QR code generated');
-  qrcode.generate(qr, { small: true });
-});
+// client.on('qr', qr => {
+//   console.log('QR code generated');
+//   qrcode.generate(qr, { small: true });
+// });
 
 client.on('ready', () => {
   botJid = client.info.wid._serialized;
@@ -223,3 +223,10 @@ client.on('message', async msg => {
 });
 
 client.initialize();
+
+const http = require('http');
+const PORT = process.env.PORT || 3000;
+http.createServer((_, res) => {
+  res.end('Bot is running');
+}).listen(PORT, '0.0.0.0');
+console.log(`Server running on port ${PORT}`);
