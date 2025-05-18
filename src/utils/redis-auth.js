e0df00b1a307e65068999c67b15a30879fc55fb7
quarj0 <owusuansahkwadwo24@gmail.com>
2025-05-18 10:15:22 +0000

    Refactor RedisAuth class to improve method organization and simplify setup logic

--- src/utils/redis-auth.js
+++ src/utils/redis-auth.js
@@ -1,3 +1,4 @@
+// utils/redis-auth.js
 const redis = require('./redis');
 
 class RedisAuth {
@@ -5,20 +6,37 @@ class RedisAuth {
     this.sessionKey = sessionKey;
   }
 
-  async setup(client) {
-    const data = await redis.get(this.sessionKey);
-    if (data) {
-      client.setAuthInfo(JSON.parse(data));
-    }
+  // Called before browser is launched
+  async beforeBrowserInitialized(opts) {
+    // you can tweak opts here if needed
   }
 
+  // Called after browser is launched
+  async afterBrowserInitialized(browser) {}
+
+  // Called before each page load
+  async beforePageLoad(page) {}
+
+  // Called on initialize, load and apply saved auth
+  async setup(client) {
+    const data = await redis.get(this.sessionKey);
+    if (data) client.setAuthInfo(JSON.parse(data));
+  }
+
+  // Called by client to load existing session
   async getAuthInfo() {
     const data = await redis.get(this.sessionKey);
     return data ? JSON.parse(data) : undefined;
   }
 
+  // Called by client to save session whenever it updates
   async saveAuthInfo(auth) {
     await redis.set(this.sessionKey, JSON.stringify(auth));
+  }
+
+  // Called by client to clear session
+  async deleteAuthInfo() {
+    await redis.del(this.sessionKey);
   }
 }
 
