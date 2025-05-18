78727b8674b1a5184e42e7394c073c16e809ae9a
quarj0 <owusuansahkwadwo24@gmail.com>
2025-05-18 10:19:11 +0000

    Add error handling for Redis connection in redis.js

--- src/utils/redis.js
+++ src/utils/redis.js
@@ -1,3 +1,4 @@
 const Redis = require('ioredis');
-const redis = new Redis(process.env.REDIS_URL); 
+const redis = new Redis(process.env.REDIS_URL);
+redis.on('error', e => console.error('Redis error', e));
 module.exports = redis;
