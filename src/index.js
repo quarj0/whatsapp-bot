9c035c1da282c4963681e27eeffef00f1916fb82
quarj0 <owusuansahkwadwo24@gmail.com>
2025-05-18 13:13:32 +0000

    fix: downgrade whatsapp-web.js to version 1.23.0 and update auth strategy to use LocalAuth

--- src/index.js
+++ src/index.js
@@ -1,4 +1,4 @@
-const { Client, MessageMedia } = require('whatsapp-web.js');
+const { Client, LocalAuth, MessageMedia } = require('whatsapp-web.js');
 const express = require('express');
 const qrcode = require('qrcode');
 const fs = require('fs');
@@ -8,7 +8,7 @@ const ms = require('ms');
 const askHF = require('./utils/gpt');
 const compression = require('compression');
 const { LRUCache } = require('lru-cache');
-const RedisAuth = require('./utils/redis-auth');
+// const RedisAuth = require('./utils/redis-auth');
 const cron = require('node-cron');
 const axios = require('axios');
 
@@ -38,10 +38,17 @@ app.listen(PORT, '0.0.0.0', () => {
 });
 
 const client = new Client({
-  authStrategy: new RedisAuth('whatsapp-session'),
+  authStrategy: new LocalAuth(),
   skipCache: true,
   puppeteer: {
-    browserWSEndpoint: process.env.BROWSER_WS_ENDPOINT,
+    headless: true,
+    args: [
+      '--no-sandbox',
+      '--disable-setuid-sandbox',
+      '--disable-dev-shm-usage',
+      '--disable-web-security',
+      '--disable-features=IsolateOrigins,site-per-process',
+    ],
   },
 });
 
