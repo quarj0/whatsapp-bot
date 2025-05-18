618ac3ff6245b339b553ded1414d88a7aafd4cee
quarj0 <owusuansahkwadwo24@gmail.com>
2025-05-18 10:10:34 +0000

    Refactor RedisAuth class to remove inheritance from Store; improve setup method and maintain session key handling

--- src/utils/redis-auth.js
+++ src/utils/redis-auth.js
@@ -1,21 +1,25 @@
-const { Store } = require('whatsapp-web.js');
 const redis = require('./redis');
 
-class RedisAuth extends Store {
+class RedisAuth {
   constructor(sessionKey = 'whatsapp-session') {
-    super();
     this.sessionKey = sessionKey;
   }
-  async setup() {
+
+  async setup(client) {
     const data = await redis.get(this.sessionKey);
-    if (data) this.saveAuthInfo(JSON.parse(data));
+    if (data) {
+      client.setAuthInfo(JSON.parse(data));
+    }
   }
-  async saveAuthInfo(auth) {
-    await redis.set(this.sessionKey, JSON.stringify(auth));
-  }
+
   async getAuthInfo() {
     const data = await redis.get(this.sessionKey);
     return data ? JSON.parse(data) : undefined;
   }
+
+  async saveAuthInfo(auth) {
+    await redis.set(this.sessionKey, JSON.stringify(auth));
+  }
 }
+
 module.exports = RedisAuth;
